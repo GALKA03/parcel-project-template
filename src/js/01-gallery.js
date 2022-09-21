f4cfe9e17d157107b660b72ec1d140a059a27e05
Galina Matvienko <galyagmgm@gmail.com>
2022-09-21 12:17:13 +0200

    бб

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,45 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import { SimpleLightbox } from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+const imgConteiner = document.querySelector('.gallery');
+const galleryMarcup = createGalaryMarcup(galleryItems);
+console.log(imgConteiner)
+imgConteiner.insertAdjacentHTML('beforeend', galleryMarcup)
+
+imgConteiner.addEventListener('click', onPicturesClick)
+
+function createGalaryMarcup(galleryItems){
+    return galleryItems
+    .map(({ preview, original, description }) => {
+      return` 
+<div class="gallery__item">
+  <a class="gallery__link" href="${ original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${ original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+}).join('')
+
+}
+
+
+
+function onPicturesClick(event) {
+    event.preventDefault();
+
+    const noEventGallary = event.target.nodeName;
+    
+    if (noEventGallary !== 'IMG') {
+        return;
+    }
+}
